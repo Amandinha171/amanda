60cdbfa1483693d16019c03a24fca8af3831dfe6
Amandinha171 <166665446+Amandinha171@users.noreply.github.com>
2025-05-28 19:02:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-26T23:29:00");
+const tempoObjetivo1 = new Date("2025-06-26T23:59:00");
 const tempoObjetivo2 = new Date("2025-11-09T23:59:00");
 const tempoObjetivo3 = new Date("2025-06-02T23:59:00");
 const tempoObjetivo4 = new Date("2025-12-25T23:59:00");
